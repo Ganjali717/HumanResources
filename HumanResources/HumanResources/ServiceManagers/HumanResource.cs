084c0359fae70055351052b7faab6418baf5b59c
Ganjali Imanov <g.imanov@qcode.pro>
2022-09-24 22:48:02 +0400

    Add constructor

--- HumanResources/HumanResources/ServiceManagers/HumanResource.cs
+++ HumanResources/HumanResources/ServiceManagers/HumanResource.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HumanResources.Models;
 
 namespace HumanResources.ServiceManagers
 {
     public class HumanResource
     {
+        public HumanResource()
+        {
+            _departments = new Department[0]; 
+        }
+
+        private Department[] _departments; 
+        public Department[] Department { get { return _departments; } }
     }
 }
